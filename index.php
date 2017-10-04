b0311e7098388fe2544b40e475d8d2c0537f58f5
Risjad Revi <reviart18@gmail.com>
2017-10-04 09:27:36 +0700

    no message

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>dafdfds</title>
+    <link rel="stylesheet" href="dist/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container">
+      <table class="table table-bordered table-responsive table-hover">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Nama panjang</th>
+            <th>Nama panggilan</th>
+            <th>Status</th>
+            <th>Alamat</th>
+            <th>Kontak</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="table-warning">
+            <td>1</td>
+            <td>Risjad M. R.</td>
+            <td>Revi</td>
+            <td>Lajang</td>
+            <td>Duri kosambi, cengkareng, jakbar.</td>
+            <td>0852-16545646565</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Zikri Wahyuzi</td>
+            <td>Zikri</td>
+            <td>Complicated</td>
+            <td>Duri kosambi, cengkareng, jakbar.</td>
+            <td>0852-5462148</td>
+          </tr>
+          <tr class="table-success">
+            <td>3</td>
+            <td>Abdul Malik</td>
+            <td>Malik</td>
+            <td>Menikah</td>
+            <td>Karang asem, solo.</td>
+            <td>0852-985462415845</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
